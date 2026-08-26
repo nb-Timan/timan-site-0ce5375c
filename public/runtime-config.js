6c1dc588cfe664922fdb019f967fc5bc12a18415
nb-Timan <nb@timan.dk>
2026-08-26 22:20:49 +0200

    Update runtime-config.js

--- public/runtime-config.js
+++ public/runtime-config.js
@@ -1,4 +1,4 @@
 window.__TIMAN_PUBLIC_CONFIG__ = Object.freeze({
   // Public browser value only. Do not add private secrets to this file.
-  VITE_CARTO_BASEMAP_KEY: "",
+  VITE_CARTO_BASEMAP_KEY: "cb1_280o_1_bf66d3d1329016e538565373",
 });
